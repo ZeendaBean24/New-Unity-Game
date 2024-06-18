12e95d0800f8a2b999c4e9c1de08f3c49c02c6cf
ZeendaBean24 <liuzeen999@gmail.com>
2024-06-18 18:55:47 -0400

    add basic enemy tracking
    with placeholder player

---
+++ Game V2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 2f;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+}

2758a0b36665f65ad9f857fff1a44ced6eed655e
ZeendaBean24 <liuzeen999@gmail.com>
2024-06-22 20:39:10 -0400

    Revert "add door unlocking"
    This reverts commit c8c2a3aae2abb59396bbf2b642b900d8a0324dde.

--- Game V2/Assets/Scripts/PlayerXP.cs
+++ Game V2/Assets/Scripts/PlayerXP.cs
@@ -2,19 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
 public class PlayerXP : MonoBehaviour
 {
     public int currentXP = 0;
-    public TextMeshProUGUI xpText; 
-    public GameObject door;
-    public Button unlockButton; 
+    public TextMeshProUGUI xpText; // Reference to the TextMeshProUGUI component
 
-    private void Start()
+    void Start()
     {
         UpdateXPUI();
-        unlockButton.onClick.AddListener(UnlockPassage);
     }
 
     public void AddXP(int amount)
@@ -28,16 +24,6 @@ public class PlayerXP : MonoBehaviour
         if (xpText != null)
         {
             xpText.text = "XP: " + currentXP;
-        }
-    }
-
-    void UnlockPassage()
-    {
-        if (currentXP >= 200)
-        {
-            currentXP -= 200;
-            UpdateXPUI();
-            Destroy(door); // Remove the door to unlock the passage
         }
     }
 }
